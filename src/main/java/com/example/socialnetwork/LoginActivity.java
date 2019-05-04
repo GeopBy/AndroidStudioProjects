3b1213ca53c96bff383277a9e7e3be13a221300d
KhoaEm <khoa.em30041997@gmai.com>
2019-05-04 18:40:46 +0700

    video 24, 25 hoàn thiện

--- src/main/java/com/example/socialnetwork/LoginActivity.java
+++ src/main/java/com/example/socialnetwork/LoginActivity.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,11 +14,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
     private Button LoginButton;
@@ -26,19 +35,28 @@ public class LoginActivity extends AppCompatActivity {
     private TextView NeedNewAccountLink;
     private ProgressDialog loadingBar;
 
-
     private FirebaseAuth mAuth;
 
+    private static final int RC_SIGN_IN = 1;
+    private GoogleApiClient mGoogleSignInClient;
+    private static final String TAG = "LoginActivity";
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mAuth=FirebaseAuth.getInstance();
-        NeedNewAccountLink =  findViewById(R.id.register_account_link);
-        UserEmail = findViewById(R.id.login_email);
-        UserPassword =  findViewById(R.id.login_password);
-        LoginButton = findViewById(R.id.login_button);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        NeedNewAccountLink = (TextView) findViewById(R.id.register_account_link);
+        UserEmail = (EditText) findViewById(R.id.login_email);
+        UserPassword = (EditText) findViewById(R.id.login_password);
+        LoginButton = (Button) findViewById(R.id.login_button);
+        googleSignInButton = (ImageView) findViewById(R.id.google_sigin_button);
         loadingBar = new ProgressDialog(this);
 
 
@@ -46,10 +64,11 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-
                 SendUserToRegisterActivity();
             }
         });
+
+
         LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
@@ -58,7 +77,102 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        // Configure Google Sign In
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("826478734615-6esbtehvf25gh3pllq4fga9qness6fce.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
+                    {
+                        Toast.makeText(LoginActivity.this, "Connection to Google Sign in failed...", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                signIn();
+            }
+        });
     }
+
+
+
+    private void signIn()
+    {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleSignInClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN)
+        {
+            loadingBar.setTitle("google Sign In");
+            loadingBar.setMessage("Please wait, while we are allowing you to login using your Google Account...");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if (result.isSuccess())
+            {
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+                Toast.makeText(this, "Please wait, while we are getting your auth result...", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(this, "Can't get Auth result.", Toast.LENGTH_SHORT).show();
+                loadingBar.dismiss();
+            }
+        }
+    }
+
+
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            Log.d(TAG, "signInWithCredential:success");
+                            SendUserToMainActivity();
+                            loadingBar.dismiss();
+                        }
+                        else
+                        {
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            String message = task.getException().toString();
+                            SendUserToLoginActivity();
+                            Toast.makeText(LoginActivity.this, "Not Authenticated : " + message, Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+                        }
+                    }
+                });
+    }
+
+
+
     @Override
     protected void onStart()
     {
@@ -66,10 +180,14 @@ public class LoginActivity extends AppCompatActivity {
 
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
-        if(currentUser != null) {
+        if(currentUser != null)
+        {
             SendUserToMainActivity();
         }
     }
+
+
+
     private void AllowingUserToLogin()
     {
         String email = UserEmail.getText().toString();
@@ -98,8 +216,8 @@ public class LoginActivity extends AppCompatActivity {
                         {
                             if(task.isSuccessful())
                             {
-
                                 SendUserToMainActivity();
+
                                 Toast.makeText(LoginActivity.this, "you are Logged In successfully.", Toast.LENGTH_SHORT).show();
                                 loadingBar.dismiss();
                             }
@@ -114,6 +232,8 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+
+
     private void SendUserToMainActivity()
     {
         Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
@@ -121,7 +241,21 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(mainIntent);
         finish();
     }
-    private void SendUserToRegisterActivity() {
+
+
+
+    private void SendUserToLoginActivity()
+    {
+        Intent mainIntent = new Intent(LoginActivity.this, LoginActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+
+
+
+    private void SendUserToRegisterActivity()
+    {
         Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(registerIntent);
     }
