6873806289bd28d82579e4babc8c1238c6b78138
KhoaEm <khoa.em30041997@gmai.com>
2019-05-05 16:26:18 +0700

    video 28 hoàn thiện, lấy liệu chuyển qua form sửa tin tức, tiếp tục

--- src/main/java/com/example/socialnetwork/ClickPostActivity.java
+++ src/main/java/com/example/socialnetwork/ClickPostActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,15 +24,17 @@ public class ClickPostActivity extends AppCompatActivity {
     private Button DeletePostButton,EditPostButton;
     private DatabaseReference clickPostRef;
     private FirebaseAuth mAuth;
+    private String PostKey, currentUserID,databaseUserID, description,image;
 
-
-    private String PostKey;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_click_post);
 
         mAuth=FirebaseAuth.getInstance();
+        currentUserID=mAuth.getCurrentUser().getUid();
+
+
         PostKey=getIntent().getExtras().get("PostKey").toString();
         clickPostRef= FirebaseDatabase.getInstance().getReference().child("Posts").child(PostKey);
         PostImage=findViewById(R.id.click_post_image);
@@ -39,14 +42,23 @@ public class ClickPostActivity extends AppCompatActivity {
         DeletePostButton=findViewById(R.id.delete_post_button);
         EditPostButton=findViewById(R.id.edit_post_button);
 
+        DeletePostButton.setVisibility(View.INVISIBLE);
+        EditPostButton.setVisibility(View.INVISIBLE);
+
         clickPostRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String description=dataSnapshot.child("description").getValue().toString();
-                String image=dataSnapshot.child("postimage").getValue().toString();
+                description=dataSnapshot.child("description").getValue().toString();
+                image=dataSnapshot.child("postimage").getValue().toString();
+                databaseUserID=dataSnapshot.child("uid").getValue().toString();
 
                 PostDescription.setText(description);
                 Picasso.get().load(image).into(PostImage);
+
+                if(currentUserID.equals(databaseUserID)){
+                    DeletePostButton.setVisibility(View.VISIBLE);
+                    EditPostButton.setVisibility(View.VISIBLE);
+                }
             }
 
             @Override
