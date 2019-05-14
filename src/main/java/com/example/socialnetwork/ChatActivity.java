9955f8639a6fc529e02e4c2f0c26f8b6423ae8e5
KhoaEm <khoa.em30041997@gmai.com>
2019-05-14 23:53:08 +0700

    video 57 design chat

--- src/main/java/com/example/socialnetwork/ChatActivity.java
+++ src/main/java/com/example/socialnetwork/ChatActivity.java
@@ -1,14 +1,38 @@
 package com.example.socialnetwork;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import androidx.appcompat.widget.Toolbar;
 
 public class ChatActivity extends AppCompatActivity {
+    
+    private Toolbar ChattoolBar;
+    private ImageButton SendMessageButton, SendImagefileButton;
+    private EditText userMessageInput;
+    private RecyclerView userMessageList;
+    private String messageReceiverID, messageReceiverName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        messageReceiverID=getIntent().getExtras().get("visit_user_id").toString();
+        messageReceiverName=getIntent().getExtras().get("userName").toString();
+
+        IntializeFields();
+    }
+
+    private void IntializeFields() {
+        ChattoolBar=findViewById(R.id.chat_bar_layout);
+        setSupportActionBar(ChattoolBar);
+
+        SendMessageButton=findViewById(R.id.send_message_button);
+        SendImagefileButton=findViewById(R.id.send_image_file_button);
+        userMessageInput=findViewById(R.id.input_message);
     }
 }
--- src/main/java/com/example/socialnetwork/FriendsActivity.java
+++ src/main/java/com/example/socialnetwork/FriendsActivity.java
@@ -88,6 +88,7 @@ public class FriendsActivity extends AppCompatActivity {
                                             if(which==1){
                                                 Intent chatintent=new Intent(FriendsActivity.this,ChatActivity.class);
                                                 chatintent.putExtra("visit_user_id",usersIDs);
+                                                chatintent.putExtra("userName",userName);
                                                 startActivity(chatintent);
 
                                             }
