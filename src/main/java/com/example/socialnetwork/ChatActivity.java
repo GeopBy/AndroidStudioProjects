0966f23b412f30a47f1fa9b9c6f58c4f6b0b10e1
KhoaEm <khoa.em30041997@gmai.com>
2019-05-15 01:27:26 +0700

    video 58, hiển thị ảnh và tên trên khung chat

--- src/main/java/com/example/socialnetwork/ChatActivity.java
+++ src/main/java/com/example/socialnetwork/ChatActivity.java
@@ -1,12 +1,27 @@
 package com.example.socialnetwork;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
 import androidx.appcompat.widget.Toolbar;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatActivity extends AppCompatActivity {
     
@@ -16,20 +31,56 @@ public class ChatActivity extends AppCompatActivity {
     private RecyclerView userMessageList;
     private String messageReceiverID, messageReceiverName;
 
+    private TextView receiverName;
+    private CircleImageView receiverProfileImage;
+    private DatabaseReference RootRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        RootRef= FirebaseDatabase.getInstance().getReference();
+
         messageReceiverID=getIntent().getExtras().get("visit_user_id").toString();
         messageReceiverName=getIntent().getExtras().get("userName").toString();
 
         IntializeFields();
+        DisplayReceiverInfo();
+    }
+
+    private void DisplayReceiverInfo() {
+        receiverName.setText(messageReceiverName);
+        RootRef.child("Users").child(messageReceiverID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    final String profileImage=dataSnapshot.child("profileimage").getValue().toString();
+                    Picasso.get().load(profileImage).placeholder(R.drawable.profile).into(receiverProfileImage);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void IntializeFields() {
         ChattoolBar=findViewById(R.id.chat_bar_layout);
         setSupportActionBar(ChattoolBar);
+
+        //hiển thị tên và ảnh đại diện của người nhận tin nhắn
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayShowCustomEnabled(true);
+        LayoutInflater layoutInflater=(LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View action_bar_view=layoutInflater.inflate(R.layout.chat_custom_bar,null);
+        actionBar.setCustomView(action_bar_view);
+
+        receiverName=findViewById(R.id.custom_profile_name);
+        receiverProfileImage=findViewById(R.id.custom_profile_image);
 
         SendMessageButton=findViewById(R.id.send_message_button);
         SendImagefileButton=findViewById(R.id.send_image_file_button);
